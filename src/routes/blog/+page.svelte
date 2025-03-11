7c65a4284d91f9e00353d998b6d589ecd4a021df
Fernando Hernandez <72260689+warmasster@users.noreply.github.com>
2025-03-11 10:30:15 +0100

    Update +page.svelte

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -3,6 +3,6 @@
     <h1 class="text-6xl">Blog</h1>
     <p class="w-[60%] mt-6 text-center">Mi blog donde encontraras varios articulos sobre varias areas de la informatica</p>
   </div>
-  <div class="border-t-2 border-slate-200 w-[100%]"></div>
+  <div class="border-t-2 mt-6 border-slate-200 w-[100%]"></div>
   
 </div>
