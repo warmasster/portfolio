4f3fe3a675c09c1d0a08649ee980a37abea34075
Fernando Hernandez <72260689+warmasster@users.noreply.github.com>
2025-03-11 10:08:40 +0100

    Update +page.svelte

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -3,8 +3,8 @@
     <h1 class="text-6xl">Blog</h1>
     <p class="w-[60%] mt-6 text-center">Mi blog donde encontraras varios articulos sobre varias areas de la informatica</p>
   </div>
-  <div class="bg-gray-200 border-2 border-gray-300 w-[30%] rounded-4xl m-5 mt-10 py-1 px-2">
-    <input class="w-[100%] focus:outline-none" placeholder="Buscar articulo . . ." type="text">
+  <div class="bg-gray-200 border-2 border-gray-300 w-[30%] rounded-full m-5 mt-10 py-1 px-2">
+    <input class="w-[100%] focus:outline-none bg-none" placeholder="Buscar articulo . . ." type="text">
   </div>
   <div class="border-t-2 border-slate-200 w-[100%]"></div>
   
