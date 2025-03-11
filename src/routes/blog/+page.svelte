6f6d989601dfb0c0c5894882d4cc0a305f3f8d86
Fernando Hernandez <72260689+warmasster@users.noreply.github.com>
2025-03-11 10:06:12 +0100

    Update +page.svelte

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,1 +1,11 @@
-<h1>Hello</h1>
+<div class="flex flex-col w-[100%] justify-center">
+  <div class="mt-8 flex flex-col items-center justify-center">
+    <h1 class="text-6xl">Blog</h1>
+    <p class="w-[60%] mt-6 text-center">Mi blog donde encontraras varios articulos sobre varias areas de la informatica</p>
+  </div>
+  <div class="bg-gray-200 border-2 border-gray-300 w-[30%] rounded-4xl m-5 mt-10 py-1 px-2">
+    <input class="w-[100%] focus:outline-none" placeholder="Buscar articulo . . ." type="text">
+  </div>
+  <div class="border-t-2 border-slate-200 w-[100%]"></div>
+  
+</div>
